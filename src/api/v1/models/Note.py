f5374e3ea7747c797a3c9e89113fad06334553a6
WilliamOnyejiaka <williamonyejiaka08062528003@gmail.com>
2022-06-02 22:48:51 +0100

    Worked on the note model and note routes .Finally know what the the 'i' in the search query means.

--- src/api/v1/models/Note.py
+++ src/api/v1/models/Note.py
@@ -1,3 +1,4 @@
+from typing import Dict, List
 from . import db 
 from datetime import datetime
 from src.modules.Serializer import Serializer
@@ -23,3 +24,28 @@ class Note:
     def get_note(id: str, user_id: str, needed_attributes: str = ['_id', 'title', 'body', 'user_id', 'created_at', 'updated_at']):
         query = notes.find_one({'_id': ObjectId(id), 'user_id': user_id})
         return Serializer(needed_attributes).serialize(query) if query else {}
+
+    @staticmethod
+    def get_notes(user_id:str,needed_attributes:List=['_id','title','body','user_id','created_at','updated_at']) -> Dict:
+        query = notes.find({'user_id':user_id}).sort('_id')
+        
+        if query:
+            return Serializer(needed_attributes).dump(list(query))
+        return {}
+    
+    @staticmethod
+    def search(user_id:str,search_param:str,needed_attributes=['_id','title','body','user_id','created_at','updated_at']) -> Dict:
+        query = notes.find({'user_id':user_id,'$or':[{
+            'body':{
+                '$regex':search_param,
+                '$options':"i"
+            }},
+            {'title':{
+                '$regex':search_param,
+                '$options':"i"
+            }}
+        ]}).sort('_id')
+
+        return Serializer(needed_attributes).dump(list(query)) if query else {}
+            
+        
--- src/api/v1/routes/note.py
+++ src/api/v1/routes/note.py
@@ -1,8 +1,9 @@
-from email import message
+from typing import List
 from flask import Blueprint,request,jsonify
 from flask_jwt_extended import jwt_required,get_jwt_identity
 from src.api.v1.models.Note import Note
 from src.modules.Serializer import Serializer
+from src.modules.Pagination import Pagination
 
 
 note = Blueprint('note',__name__,url_prefix="/api/v1/note")
@@ -23,6 +24,33 @@ def add_note():
 def get_note(id:str):
     user_id = get_jwt_identity()
     user_notes = Note.get_note(id,user_id)
-    return jsonify({'error':True,'message':user_notes}),200
+    return jsonify({'error':False,'message':user_notes}),200
 
+@note.get("/get_notes")
+@jwt_required()
+def get_notes():
+    user_id = get_jwt_identity()
+    user_notes = Note.get_notes(user_id)
+    return jsonify({'error':False,'data':user_notes}),200
+
+@note.get("/pagination/note_pagination")
+@jwt_required()
+def note_pagination():
+    try:
+        page = int(request.args.get('page', 1))
+        limit = int(request.args.get('limit',10))
+    except:
+        return jsonify({'error':True,'message':"page and limit must be integers"}),400
+    
+    user_id = get_jwt_identity()
+    pagination_results = Pagination(Note.get_notes(user_id),page,limit).meta_data()
+    return jsonify({'error':False,'data':pagination_results}),200
+    
+
+@note.get("/search/<search_param>")
+@jwt_required()
+def search(search_param):
+    user_id = get_jwt_identity()
+    search_results:List = Note.search(user_id,search_param)
+    return jsonify({'error':False,'data':search_results}),200
 
